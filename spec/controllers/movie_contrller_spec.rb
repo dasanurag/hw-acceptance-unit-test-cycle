be2a09da7b5537cd59099150699fc1e5d93b8d8e
anurag <das.anurag2012@gmail.com>
2018-10-06 20:48:52 -0500

    Added rspec tests

--- spec/controllers/movie_contrller_spec.rb
+++ spec/controllers/movie_contrller_spec.rb
@@ -2,24 +2,30 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe MoviesController, type: :controller do
+
     
-    it 'destroy a movie' do
-        movie = Movie.create(:title => "Titanic", :director => "James Cameron")
-        delete :destroy, :id => movie.id
-        expect{Movie.find(movie.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    describe "destroy" do
+      it 'destroy a movie' do
+          movie = Movie.create(:title => "Titanic", :director => "James Cameron")
+          delete :destroy, :id => movie.id
+          expect{Movie.find(movie.id)}.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
     
-    it 'tests creation of a movie' do
-        post :create, :movie => { :title => "T", :director => "D", :rating => "R", :release_date =>"09/09/2010"}
+    describe "create" do
+      it 'tests if movie is created' do
+        movie = {:title => 'Star Wars', :director => 'George Lucas', :rating =>'PG', :description => 'Great Movie', :release_date => "5/25/1977"}
+        post :create, movie: movie
         expect(flash[:notice]).to be_present
         expect(response).to redirect_to(movies_path)
+      end
     end
-    
-   it 'should view the index' do
-	get :index
-	expect(response).to render_template('index')
+
+   describe "index" do
+     it 'test to view the index' do
+	  get :index
+	  expect(response).to render_template('index')
+     end
    end
-
-   it 
-
+   
 end
